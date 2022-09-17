b699d8c1a2ea826cfd76b28c3b243ccb10f164e8
Gabriel Bello <gabego@yahoo.com>
2022-09-17 14:49:59 -0500

    version 2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import quotes from './json/quotes.json'
+import QuoteBox from './components/QuoteBox'
+import color from './utils/color'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  
 
+  const getIndexRandom = arr => Math.floor(Math.random() * arr.length)
+
+  //llama funcion para optener Indice Random por primera vez
+  const firstQuote = quotes[getIndexRandom(quotes)]
+  const firstColor = color[getIndexRandom(color)]
+
+  //Se guarda el estado de cada dato
+  const [randomQuote, setRandomQuote] = useState(firstQuote)
+  const [randomColor, setRandomColor] = useState(firstColor)
+
+  const backgroundObject = {
+    backgroundColor: randomColor
+  }
+//Calculo de Quote y Color Aleatorio 1:32:19
+  const getRandomAll =() => {
+    setRandomQuote(quotes[getIndexRandom(quotes)])
+    setRandomColor(color[getIndexRandom(color)])
+  }
   return (
-    <div className="App">
-      <h1>hola</h1>
+    <div style={backgroundObject} className="App">
+      <QuoteBox
+       randomQuote={randomQuote}
+       randomColor={randomColor}
+       getRandomAll={getRandomAll} />
     </div>
   )
 }
